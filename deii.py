78faac40c4ab4e2f8dfcb968c1ffbc31737b4c3e
dhananjayan111 <73292320+dhananjayan111@users.noreply.github.com>
2020-11-09 19:06:08 +0530

    Update deii.py

--- deii.py
+++ deii.py
@@ -16,11 +16,9 @@ def colorizer(img):
     #img = cv2.imread(img)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    prototxt = "./models/models_colorization_deploy_v2.prototxt"
+    prototxt = "http://eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2.caffemodel" 
     model = "./models/colorization_release_v2.caffemodel"
     points = "./models/pts_in_hull.npy"
-    for path in ["./models/colorization_release_v2.caffemodel"]
-    print (os.path.exists(path))
     net = cv2.dnn.readNetFromCaffe(prototxt, model)
     pts = np.load(points)
     class8 = net.getLayerId("class8_ab")
